3308e53c6df05f3a6d2bde1ddcf5604c779210d0
gllmar <guillaume@artificiel.org>
2016-04-25 19:00:44 -0400

    ajout de case gestion + update css + briser le support slider  bb10+ ajout progress +

--- interface/pages/loadInterface.js
+++ interface/pages/loadInterface.js
@@ -1,46 +1,115 @@
+(function() {
+    console.log('starting....')
+    rhizome.start(function(err) {
+        if (err) throw err
+        rhizome.send('/sys/subscribe', ['/'])
+    })
+    rhizome.on('message', function(addr, args) {
+
+        switch (document.getElementById(addr).className) {
+            case "hFader":
+                document.getElementById(addr).value = (args[0]);
+                document.getElementById(addr + ".label").innerHTML = parseFloat(args[0]).toPrecision(5);
+                break;
+            case "progress":
+                document.getElementById(addr).value = (args[0]);
+                document.getElementById(addr+".label").innerHTML = parseFloat(args[0]).toPrecision(3);
+                break;
+
+            case "label":
+                document.getElementById(addr + ".label").innerHTML = parseFloat(args[0]).toPrecision(3);
+                break;
+
+            case "push":
+                  break;
+
+            default:
+        }
+    })
+    rhizome.on('connected', function() {
+        console.log('connected')
+    })
+})();
+
 function loadInterface() {
-  console.log(myInterface.interface.length);
-   for (i = 0; i < myInterface.interface.length; i++) {
+    console.log(myInterface.interface.length);
+    for (i = 0; i < myInterface.interface.length; i++) {
 
-     if (myInterface.interface[i].class == "title"){
-        var x = document.createElement("SPAN");
-        x.innerHTML = myInterface.interface[i].name ;
-        document.body.appendChild(x);
-        var w = document.createElement("BR");
-        document.body.appendChild(w);
-     }
+        switch (myInterface.interface[i].class) {
+            case "title":
+                var x = document.createElement("SPAN");
+                x.innerHTML = myInterface.interface[i].name;
+                x.className = myInterface.interface[i].class;
+                document.body.appendChild(x);
+                var w = document.createElement("BR");
+                document.body.appendChild(w);
+                break;
 
-     if (myInterface.interface[i].class == "hFader"){
+            case "label":
+                var x = document.createElement("SPAN");
+                var y = document.createElement("SPAN")
+                x.innerHTML = "/" + myInterface.interface[i].name + " ";
+                x.id = '/' + myInterface.interface[i].name;
+                x.className = myInterface.interface[i].class;
+                y.className = myInterface.interface[i].class;
+                y.id = '/' + myInterface.interface[i].name + ".label";
+                document.body.appendChild(x);
+                document.body.appendChild(y);
+                var w = document.createElement("BR");
+                document.body.appendChild(w);
+                break;
 
-     var x = document.createElement("INPUT");
-     var y = document.createElement("SPAN");
-     var z = document.createElement("SPAN");
-     x.type = "range";
-     x.id = '/'+myInterface.interface[i].name+".input";
-     x.setAttribute("class", myInterface.interface[i].class)
-     x.setAttribute("min", myInterface.interface[i].min);
-     x.setAttribute("max", myInterface.interface[i].max);
-     x.setAttribute("step", myInterface.interface[i].step);
-     x.setAttribute("oninput", "rhizome.send("+"'/"+myInterface.interface[i].name+"'"+",[parseFloat(value)])")
-     y.innerHTML = '/'+myInterface.interface[i].name+" ";
-     z.id = '/'+myInterface.interface[i].name+".label"
+            case "hFader":
+                var x = document.createElement("INPUT");
+                var y = document.createElement("SPAN");
+                var z = document.createElement("SPAN");
+                x.type = "range";
+                x.id = '/' + myInterface.interface[i].name;
+                x.className = myInterface.interface[i].class;
+                x.min = myInterface.interface[i].min;
+                x.max = myInterface.interface[i].max;
+                x.step = myInterface.interface[i].step;
+                x.setAttribute("oninput", "rhizome.send(" + "'/" + myInterface.interface[i].name + "'" + ",[parseFloat(value)])")
+                y.innerHTML = '/' + myInterface.interface[i].name + " ";
+                z.id = '/' + myInterface.interface[i].name + ".label"
+                document.body.appendChild(y);
+                document.body.appendChild(z);
+                var w = document.createElement("BR");
+                document.body.appendChild(w);
+                document.body.appendChild(x);
+
+                break;
+
+            case "push":
+                var x = document.createElement("BUTTON");
+                x.id = '/' + myInterface.interface[i].name;
+                x.setAttribute("class", myInterface.interface[i].class)
+                x.setAttribute("onclick", "rhizome.send(" + "'/" + myInterface.interface[i].name + "'" + ",[parseFloat(1)])")
+                x.innerHTML = '/' + myInterface.interface[i].name + " ";
+                document.body.appendChild(x);
+                break;
+
+              case "progress":
+                  var x = document.createElement("PROGRESS");
+                  var y = document.createElement("SPAN");
+                  var z = document.createElement("SPAN");
+                  x.id = '/' + myInterface.interface[i].name;
+                  x.className = myInterface.interface[i].class;
+                  x.min = myInterface.interface[i].min;
+                  x.max = myInterface.interface[i].max;
+                  y.innerHTML = '/' + myInterface.interface[i].name + " ";
+                  z.id = '/' + myInterface.interface[i].name +".label"
+                  document.body.appendChild(y);
+                  document.body.appendChild(z)
+                  document.body.appendChild(x);
+                  var w = document.createElement("BR");
+                  document.body.appendChild(w);
+
+                  break;
 
 
-    document.body.appendChild(y);
-    document.body.appendChild(z);
-    var w = document.createElement("BR");
-    document.body.appendChild(w);
-      document.body.appendChild(x);
+            default:
 
-}
-
- else if (myInterface.interface[i].class == "push"){
-  var x = document.createElement("BUTTON");
-  x.id = '/'+myInterface.interface[i].name+".input";
-  x.setAttribute("class", myInterface.interface[i].class)
-  x.setAttribute("onclick","rhizome.send("+"'/"+myInterface.interface[i].name+"'"+",[parseFloat(1)])")
-  x.innerHTML = '/'+myInterface.interface[i].name+" ";
-  document.body.appendChild(x);
-  }
-}
+        }
+    }
 }
